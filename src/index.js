9a412409cf4fa75686112bac15e20b8aa8a63516
Eric Low <happinesseric525@gmail.com>
2017-09-01 11:49:50 -0400

    complete react-redux simple project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 
 import App from './components/App';
 
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+//import * as actions from './actions';
+
+// console.log(store.getState());
+// const unsubscribe = store.subscribe( () => console.log(store.getState()));
+// store.dispatch(actions.increment());
+// store.dispatch(actions.increment());
+// store.dispatch(actions.decrement());
+// store.dispatch(actions.setColor([100,100,100]));
+//
+// unsubscribe();
+//
+// //does not work the next dispatch
+//
+// store.dispatch(actions.setColor([200,100,200]));
+
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+       <App/>
+  </Provider>,
   document.getElementById('root')
 );
